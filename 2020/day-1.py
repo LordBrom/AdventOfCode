71e6464d73498247843139c6e3ef1fccc6e34ccc
Nate Mills <mills.nate@gmail.com>
2020-11-30 21:24:52 -0800

    Solving day 1

--- 2020/day-1.py
+++ 2020/day-1.py
@@ -1,9 +1,17 @@
-#import math
-
-def do_something(inputText):
-	result = inputText
-	return result
-
+inputsArray = []
 inputText = input("Input: ")
+while inputText != '':
+	inputsArray.append(int(inputText))
+	inputText = input("Input: ")
 
-print(do_something(inputText))
+for i in range(len(inputsArray)):
+	for j in range(i, len(inputsArray)):
+		for k in range(j, len(inputsArray)):
+			if inputsArray[i] + inputsArray[j] + inputsArray[k] == 2020:
+				print(inputsArray[i])
+				print(inputsArray[j])
+				print(inputsArray[k])
+				print(inputsArray[i] * inputsArray[j] * inputsArray[k])
+
+#print(do_something(inputText))
+
