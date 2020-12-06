f2bf871cda767406d106902f2b6985184d55a625
Nate Mills <mills.nate@gmail.com>
2020-12-05 20:57:47 -0800

    Day 6

---
+++ 2020/day-6.py
@@ -0,0 +1,44 @@
+
+def count_group_part1(group):
+	found = []
+	count = 0
+	for person in group:
+		for question in person:
+			if not question in found:
+				count += 1
+				found.append(question)
+
+	return count
+
+def count_group_part2(group):
+	found = []
+	first = True
+	for person in group:
+		check = []
+		for question in person:
+			check.append(question)
+		newFound = []
+		if first:
+			newFound = check
+			first = False
+		else:
+			for q in found:
+				if q in check:
+					newFound.append(q)
+
+		found = newFound
+
+
+	return len(found)
+
+
+inFile = open("day-6.in", "r").read().split("\n\n")
+inFile.pop()
+
+total = 0
+
+for group in inFile:
+	splitGroup = group.split("\n")
+	total += count_group_part2(splitGroup)
+
+print(total)
