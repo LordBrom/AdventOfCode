e59eae51caaeb543a90235a27050e8428275b0a5
Nate Mills <mills.nate@gmail.com>
2020-12-25 09:57:38 -0800

    Split day 1.

--- 2020/day1-2.py
+++ 2020/day1-2.py
@@ -3,8 +3,6 @@ inputsArray.pop()
 
 for i in range(len(inputsArray)):
 	for j in range(i, len(inputsArray)):
-		if int(inputsArray[i]) + int(inputsArray[j]) == 2020:
-			print("Part 1: " + str(int(inputsArray[i]) * int(inputsArray[j])))
 		for k in range(j, len(inputsArray)):
 			if int(inputsArray[i]) + int(inputsArray[j]) + int(inputsArray[k]) == 2020:
-				print("Part 2: " + str(int(inputsArray[i]) * int(inputsArray[j]) * int(inputsArray[k])))
+				print(int(inputsArray[i]) * int(inputsArray[j]) * int(inputsArray[k]))
