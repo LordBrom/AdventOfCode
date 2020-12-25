373d5265515fe59f577b1a11f9e280fa3dca5411
Nate Mills <mills.nate@gmail.com>
2020-12-25 09:59:05 -0800

    Split day 2

--- 2020/day2-2.py
+++ 2020/day2-2.py
@@ -1,6 +1,6 @@
 import re
 
-def is_valid_password_part_2(input):
+def is_valid_password(input):
 	m = re.search('([0-9]+)-([0-9]+) ([a-z]+): ([a-z]+)', input)
 	minRange = m.group(1)
 	maxRange = m.group(2)
@@ -17,30 +17,11 @@ def is_valid_password_part_2(input):
 
 	return letterCount == 1
 
-def is_valid_password_part_1(input):
-	m = re.search('([0-9]+)-([0-9]+) ([a-z]+): ([a-z]+)', input)
-	minRange = m.group(1)
-	maxRange = m.group(2)
-	letter   = m.group(3)
-	password = m.group(4)
-
-	letterCount = 0
-	for i in password:
-		if i == letter:
-			letterCount += 1
-	if letterCount >= int(minRange) and letterCount <= int(maxRange):
-		return True
-	return False
-
-count1 = 0
-count2 = 0
+count = 0
 inputText = open("day2.in", "r").read().split("\n")
 inputText.pop()
 for text in inputText:
-	if is_valid_password_part_1(text):
-		count1 += 1
-	if is_valid_password_part_2(text):
-		count2 += 1
+	if is_valid_password(text):
+		count += 1
 
-print("part 1:" + str(count1))
-print("part 2:" + str(count2))
+print(count)
